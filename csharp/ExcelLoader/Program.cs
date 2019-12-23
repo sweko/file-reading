0b04d4b0fdea518066f191bc168ef230a22f6658
Wekoslav Stefanovski <swekster@gmail.com>
2019-12-23 20:17:21 +0100

    Added excel loader

---
+++ csharp/ExcelLoader/Program.cs
@@ -0,0 +1,37 @@
+﻿using ExcelDataReader;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace ExcelLoader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            var sw = Stopwatch.StartNew();
+            int rowCount = 0;
+            double minYear = double.MaxValue;
+            using var stream = File.OpenRead("toread.xlsx");
+            using var reader = ExcelReaderFactory.CreateReader(stream);
+            //skip headers
+            reader.Read();
+            while (reader.Read())
+            {
+                rowCount += 1;
+                //int.TryParse(reader.GetValue(6)?.ToString()??"", out int year);
+                //if (year < minYear) minYear = year;
+                //if (rowCount % 10000 == 0)
+                //{
+                //    Console.WriteLine($"Processed {rowCount} rows, minimal year is {minYear}. Continuing.");
+                //}
+            }
+
+            sw.Stop();
+            Console.WriteLine($"Processed {rowCount} rows. Finished.");
+            Console.WriteLine($"{sw.ElapsedMilliseconds}ms elapsed");
+        }
+    }
+}
