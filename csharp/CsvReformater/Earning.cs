a65e54187b5d47f89999a5eda890f58d4cdca4e2
Wekoslav Stefanovski <swekster@gmail.com>
2019-12-23 01:13:48 +0100

    CSharp commit

---
+++ csharp/CsvReformater/Earning.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsvReformater
+{
+    class Earning
+    {
+        public string IdEarning;
+        public string CreatedAt;
+        public string UpdatedAt;
+        public string UpdatedBy;
+        public string CreatedBy;
+        public string Account_id;
+        public string PeriodEndYear;
+        public string TaxYear;
+        public string PensionEarningsRaw;
+        public string PensionEarningsAdjustment;
+        public string HoursRaw;
+        public string HoursAdjustment;
+        public string ServiceRaw;
+        public string ServiceAdjustment;
+        public string EarningNotes;
+        public string Plan_id;
+        public string ImportTransaction_id;
+        public string CustomOne;
+        public string CustomTwo;
+        public string CustomThree;
+        public string CustomFour;
+        public string HoursAdjustmentNote;
+        public string ServiceAdjustmentNote;
+        public string StartDate;
+        public string EndDate;
+        public string Employer_id;
+        public string IsDeleted;
+        public string JobGroup;
+    }
+}
